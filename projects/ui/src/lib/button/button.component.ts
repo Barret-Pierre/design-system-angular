779ea381626af0aa887dd5d7e6f69a9582e490ea
Barret-Pierre <pierrealexandrebarret.pro@gmail.com>
2025-04-08 22:41:20 +0200

    feat(button): Use ui button into app

--- projects/ui/src/lib/button/button.component.ts
+++ projects/ui/src/lib/button/button.component.ts
@@ -1,7 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ui-button',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
